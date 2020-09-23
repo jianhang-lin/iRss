531c818183f43d400260ebc35f697707ec8eebb3
JianhangLin <linjianhang1010@gmail.com>
2020-09-23 10:50:43 +0800

    init iris server

---
+++ irss-server/src/main/java/com/grant/irss/IrssServerApplication.java
@@ -0,0 +1,13 @@
+package com.grant.irss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrssServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IrssServerApplication.class, args);
+    }
+
+}
